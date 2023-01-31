e9f1c4f42001cb97edea74e409a269d0970aa55a
YinCheng <eason010630@gmail.com>
2023-01-31 13:09:54 +0800

    0.7.0
    add invite me

---
+++ src/commands/info/invite.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Guild} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('invite')
+        .setNameLocalizations({
+            "zh-TW":"邀請我"
+        })
+        .setDescription('invite me join to your server')
+        .setDescriptionLocalizations({
+            "zh-TW":"邀請我加入你的伺服器"
+        }),
+
+    async execute(interaction, client) {
+        const invite_embed = new EmbedBuilder()
+            .setTitle('<:bot:1069839984007331961> 邀請我')
+            .setColor('Random')
+            .setDescription('**歡迎加入我，讓你的伺服器添加一些風采**')
+            .setFooter({
+                iconURL: client.user.displayAvatarURL(),
+                text: `YINLA • 讓你的伺服器添加一些風采`
+            })
+        const invite_button = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setEmoji('<:bot:1069839984007331961>')
+                    .setLabel('點擊邀請我')
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://discord.com/api/oauth2/authorize?client_id=914150570250625044&permissions=8&scope=bot%20applications.commands')
+            )
+            .addComponents(
+                new ButtonBuilder()
+                    .setEmoji('<:yinla_v2:980430954063593523>')
+                    .setLabel('加入支援伺服器')
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://discord.gg/mnCHdBbh65')
+            )
+
+        await interaction.reply({
+            embeds:[invite_embed],
+            components:[invite_button]
+        })
+    }
+}
