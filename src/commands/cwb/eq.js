f15fa9aa66d9082935bd450fff1ec26bfbe17e0c
YinCheng0106 <eason010630@gmail.com>
2023-07-30 17:25:18 +0800

    0.12.0

--- src/commands/cwb/eq.js
+++ src/commands/cwb/eq.js
@@ -2,24 +2,46 @@ const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Embed
 const axios = require('axios');
 
 module.exports = {
-    data : new SlashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName('earthquake_report')
         .setNameLocalizations({
-            "zh-TW" : "編號地震報告",
+            "zh-TW": "地震報告"
         })
-        .setDescription('檢視 最新有編號 的地震'),
-    
-    async execute (interaction, client) {
-        const eqResult = await axios.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization=${process.env.cwb_key}`);
+        .setDescription('檢視 地震報告')
+        .addStringOption(option => (
+            option
+                .setName("type")
+                .setNameLocalizations({
+                    "zh-TW": "類型",
+                })
+                .setDescription("有編號 or 無編號"))
+                .setDescriptionLocalizations({
+                    "zh-TW": "有編號 or 無編號",
+                })
+                .setRequired(true)
+                .addChoices(
+                    { name: '有編號', value: '有編號' },
+                    { name: '無編號', value: '無編號' },
+                )
+        ),
+
+    async execute(interaction, client) {
+        
+        if(interaction.options.getString('type') == '無編號'){
+            cwbID = 'E-A0016-001'
+        } else {
+            cwbID = 'E-A0015-001'
+        }
+
+        const eqResult = await axios.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/${cwbID}?Authorization=${process.env.cwb_key}`);
         const { records } = eqResult.data;
         const Earthquake = records.Earthquake[0];
-
         const No = String(Earthquake.EarthquakeNo);
         const Color = {
-            綠色 : "Green",
-            黃色 : "Yellow",
-            橙色 : "Orange",
-            紅色 : "Red",
+            綠色: "Green",
+            黃色: "Yellow",
+            橙色: "Orange",
+            紅色: "Red",
         };
         const Content = Earthquake.ReportContent.substring(11);
 
@@ -67,58 +89,62 @@ module.exports = {
                     .setURL(Earthquake.Web),
             ]);
 
-        
-        
+
+
         const Depth = String(Earthquake.EarthquakeInfo.FocalDepth);
         const Location = Earthquake.EarthquakeInfo.Epicenter.Location;
         const Magnitude = String(Earthquake.EarthquakeInfo.EarthquakeMagnitude.MagnitudeValue);
 
         embed = new EmbedBuilder()
-        .setAuthor({
-            name:"地震報告",
-            iconURL:"https://i.imgur.com/qIxk1H1.png"
-        })
-        .setDescription(Content)
-        .setColor(Color[Earthquake.ReportColor])
-        .setImage(Image)
-        .addFields([
-            {
-                name : '編號',
-                value : `${No % 1000 == 0 ? "無（小區域有感地震）" : No}`,
-                inline : true
-            },
-            {
-                name : '發生時間',
-                value : `<t:${~~(Time.getTime() / 1000)}:D>\n<t:${~~(Time.getTime() / 1000)}:T>\n__(<t:${~~(Time.getTime() / 1000)}:R>)__`,
-                inline : true
-            },
-            {
-                name : '震央',
-                value : Location,
-                inline : true
-            },
-            {
-                name : '規模',
-                value : "芮氏 " + Magnitude,
-                inline : true
-            },
-            {
-                name : '深度',
-                value : Depth + " 公里",
-                inline : true
-            },
-        ])
-        .setFooter({ text: "交通部中央氣象局", iconURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/ROC_Central_Weather_Bureau.svg/1200px-ROC_Central_Weather_Bureau.svg.png" })
-        .setTimestamp(Time);
+            .setAuthor({
+                name: "地震報告",
+                iconURL: "https://i.imgur.com/qIxk1H1.png"
+            })
+            .setDescription(Content)
+            .setColor(Color[Earthquake.ReportColor])
+            .setImage(Image)
+            .addFields([
+                {
+                    name: '編號',
+                    value: `${No % 1000 == 0 ? "無（小區域有感地震）" : No}`,
+                    inline: true
+                },
+                {
+                    name: '發生時間',
+                    value: `<t:${~~(Time.getTime() / 1000)}:D>\n<t:${~~(Time.getTime() / 1000)}:T>\n__(<t:${~~(Time.getTime() / 1000)}:R>)__`,
+                    inline: true
+                },
+                {
+                    name: '震央',
+                    value: Location,
+                    inline: true
+                },
+                {
+                    name: '規模',
+                    value: "芮氏 " + Magnitude,
+                    inline: true
+                },
+                {
+                    name: '深度',
+                    value: Depth + " 公里",
+                    inline: true
+                },
+            ])
+            .setFooter({ text: "交通部中央氣象局", iconURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/ROC_Central_Weather_Bureau.svg/1200px-ROC_Central_Weather_Bureau.svg.png" })
+            .setTimestamp(Time);
 
         Earthquake.Intensity.ShakingArea
-        .filter(v => !v.InfoStatus)
-        .forEach(ShakingArea => embed.addFields({ name: ShakingArea.AreaDesc, value: ShakingArea.CountyName }));
+            .filter(v => !v.InfoStatus)
+            .forEach(ShakingArea => embed.addFields({ name: ShakingArea.AreaDesc, value: ShakingArea.CountyName }));
 
-        await interaction.reply({
+        const WaitMessage = await interaction.deferReply({
+            fetchReply: true,
+            ephemeral: true
+        });
+
+        const SuccessMessage = await interaction.editReply({
             embeds: [embed],
             components: [url],
-            ephemeral: true
         });
     }
 }
