6be9ec18d18680e0814c66cd547840c717b5e84e
YinCheng0106 <eason010630@gmail.com>
2024-04-28 22:22:47 +0800

    0.16.1

--- src/events/guild/LinktoEmbed.js
+++ src/events/guild/LinktoEmbed.js
@@ -7,7 +7,7 @@ module.exports = {
         if(interaction.content.startsWith('https://x.com')){
             const message = interaction.content.replace("x.com", "vxtwitter.com");
             await interaction.reply({
-                content: `<@${interaction.author.id}>傳了一則推[超連結](${message})`,
+                content: `<@${interaction.author.id}>傳了一則推 [超連結](${message})`,
                 allowedMentions: {
                     repliedUser: false
                 }
@@ -15,7 +15,7 @@ module.exports = {
         } else if(interaction.content.startsWith('https://twitter.com')){
             const message = interaction.content.replace("twitter.com", "vxtwitter.com");
             await interaction.reply({
-                content: `<@${interaction.author.id}> 傳了一則推[超連結](${message})`,
+                content: `<@${interaction.author.id}> 傳了一則推 [超連結](${message})`,
                 allowedMentions: {
                     repliedUser: false
                 }
@@ -23,7 +23,7 @@ module.exports = {
         } else if(interaction.content.startsWith('https://www.instagram.com')){
             const message = interaction.content.replace("instagram.com", "ddinstagram.com");
             await interaction.reply({
-                content: `<@${interaction.author.id}> 傳了一則文[超連結](${message})`,
+                content: `<@${interaction.author.id}> 傳了一則IG文 [超連結](${message})`,
                 allowedMentions: {
                     repliedUser: false
                 }
