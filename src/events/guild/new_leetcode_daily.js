37c33065faa506963ee5b35771d5277835900546
YinCheng <eason010630@gmail.com>
2025-05-24 20:33:38 +0800

    feat: leetcode daily push channel

---
+++ src/events/guild/new_leetcode_daily.js
@@ -0,0 +1,96 @@
+const cron = require('cron');
+const { ThumbnailBuilder, TextDisplayBuilder, SectionBuilder, 
+        ContainerBuilder, SeparatorSpacingSize, MessageFlags,
+        ButtonBuilder, ButtonStyle, ActionRowBuilder, Colors } = require('discord.js');
+const html2md = require('html-to-md');
+
+const fetchLeetCodeDaily = async () => {
+    const response = await fetch('https://alfa-leetcode-api.onrender.com/daily', { method: 'GET' });
+    const data = await response.json();
+    if(data) return data;
+    else return;
+}
+
+const difficulty_color = (difficulty) => {
+    if(difficulty === "Easy") return Colors.Green
+    else if(difficulty === "Medium") return Colors.Yellow
+    else if(difficulty === "Hard") return Colors.Red
+    else return Colors.Random
+}
+
+module.exports = {
+    name: 'ready',
+    once: false,
+    async execute(client) {
+        const job = new cron.CronJob("0/15 * * * * *", async function () {
+            const data = await fetchLeetCodeDaily();
+            const leetcode_channel = client.channels.cache.get(process.env.leetcode_channel);
+            if (!leetcode_channel) return;
+
+            const leetcode_logo = new ThumbnailBuilder()
+                .setURL('https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png')
+            
+            const leetcode_title = new TextDisplayBuilder()
+                .setContent([
+                    `**LeetCode Daily**`,
+                    `# [[${data.questionFrontendId}] ${data.questionTitle}](${data.questionLink})`,
+                ].join('\n'))
+
+            const leetcode_header = new SectionBuilder()
+                .setThumbnailAccessory(leetcode_logo)
+                .addTextDisplayComponents(leetcode_title)
+
+            const leetcode_description = new TextDisplayBuilder()
+                .setContent(
+                    `${html2md(data.question).replace(/<sub>(.*?)<\/sub>/g, '_$1')}`
+                )
+
+            const leetcode_error_msg = new TextDisplayBuilder()
+                .setContent([
+                    `# 發生錯誤！`,
+                    `請回報`,
+                ].join('\n'))
+
+            const leetcode_footer = new TextDisplayBuilder()
+                .setContent(`-# 此為測試功能，如果有誤請回報`)
+
+            const leetcode_button = new ButtonBuilder()
+                .setLabel('Question')
+                .setStyle(ButtonStyle.Link)
+                .setURL(data.questionLink)
+
+            const leetcode_url = new ActionRowBuilder()
+                .addComponents(leetcode_button)
+
+            const leetcode_container = new ContainerBuilder()
+
+            if(data){
+                leetcode_container
+                    .setId(1)
+                    .setSpoiler(false)
+                    .setAccentColor(difficulty_color(data.difficulty))
+                    .addSectionComponents(leetcode_header)
+                    .addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Small))
+                    .addTextDisplayComponents(leetcode_description)
+                    .addActionRowComponents(leetcode_url)
+                    .addTextDisplayComponents(leetcode_footer)
+            } else {
+                leetcode_container
+                    .setId(1)
+                    .setSpoiler(false)
+                    .addTextDisplayComponents(leetcode_error_msg)
+                    .addTextDisplayComponents(leetcode_footer)
+            }
+
+            leetcode_channel.send({
+                silent: true,
+                components: [ leetcode_container ],
+                flags: [ MessageFlags.IsComponentsV2 ],
+            });
+            console.log('[發布] LeetCode Daily');
+        }, null, true, 'Asia/Taipei');
+
+        job.start();
+        console.log('[啟動] LeetCode Daily任務');
+    }
+}
